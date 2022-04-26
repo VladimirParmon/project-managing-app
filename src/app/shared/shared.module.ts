73a513d3176fc6b52bee7eea762652beaae679fc
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-26 18:08:08 +0300

    add greeting page v1.0

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatToolbarModule, FormsModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, FormsModule],
 })
 export class SharedModule {}
