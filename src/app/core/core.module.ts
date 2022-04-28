0370967ee0be96d0fd1595e75c1eb39a39aa102d
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-28 20:49:15 +0300

    fix: broken imports and app prefix

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
 import { GreetingPageComponent } from './pages/greeting-page/greeting-page.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { Page404Component } from './pages/page404/page404.component';
 
-
 @NgModule({
-  declarations: [
-    GreetingPageComponent,
-    HeaderComponent,
-    FooterComponent,
-    Page404Component
-  ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [GreetingPageComponent, Page404Component],
+  imports: [CommonModule, CoreRoutingModule],
 })
-export class CoreModule { }
+export class CoreModule {}
