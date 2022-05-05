948020a7885993c5e3e7cc540fb434f6a8a437ef
@shadowinhaze <shadowinhaze@gmail.com>
2022-05-05 15:06:52 +0200

    minor: edit top app.module with connected ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { metaReducers, reducers } from './reducers';
+import { interceptorProviders } from './core/interceptors';
+import { reducers, metaReducers, effects } from './redux';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-    }),
+    HttpClientModule,
+    StoreModule.forRoot(reducers, metaReducers),
+    EffectsModule.forRoot(effects),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([]),
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [interceptorProviders],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
