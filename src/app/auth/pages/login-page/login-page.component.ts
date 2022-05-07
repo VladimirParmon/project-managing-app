c2854829d0be01607e6fe106c31d792fcb88d10b
@shadowinhaze <shadowinhaze@gmail.com>
2022-05-07 09:39:34 +0200

    fix: add unsubscription to Login Page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
-import { selectAuthErrorMessage } from 'src/app/redux/selectors/user.selectors';
+import { Subscription } from 'rxjs';
+import { selectAuthErrorMessage } from 'src/app/redux';
 
 @Component({
   selector: 'ma-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   private AuthUserError$ = this.store.select(selectAuthErrorMessage);
+
+  private subscription = new Subscription();
 
   constructor(private msb: MatSnackBar, private store: Store) {}
 
   ngOnInit(): void {
-    this.AuthUserError$.subscribe((msg) => {
-      if (msg && msg.length > 0) {
-        this.msb.open(msg, 'Understood', { duration: 10000, panelClass: ['mat-snack-bar-custom'] });
-      }
-    });
+    this.subscription.add(
+      this.AuthUserError$.subscribe((msg) => {
+        if (msg && msg.length > 0) {
+          this.msb.open(msg, 'Understood', {
+            duration: 10000,
+            panelClass: ['mat-snack-bar-custom'],
+          });
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
