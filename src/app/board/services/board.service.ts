3014c4dc70925da018e73efb09e87fb1dd6e1192
Ilya758 <subscriber6436@gmail.com>
2022-05-09 17:49:51 +0300

    feat: implemented crud service logic for component BoardPage

--- src/app/board/services/board.service.ts
+++ src/app/board/services/board.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TBoards } from 'src/app/redux/models/store.model';
-import { IBoard } from 'src/app/shared/models/board.model';
+import { TBoards, TColumns } from 'src/app/redux/models/store.model';
+import { IBoard, IColumn } from 'src/app/shared/models/board.model';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +19,21 @@ export class BoardService {
 
   deleteBoard(id: string) {
     return this.http.delete<IBoard>(`boards/${id}`);
+  }
+
+  fetchColumnsById(id: string) {
+    return this.http.get<TColumns>(`boards/${id}/columns`);
+  }
+
+  createColumn(id: string, title: string, order: number) {
+    return this.http.post<IColumn>(`boards/${id}/columns`, { title, order });
+  }
+
+  deleteColumn(boardId: string, columnId: string) {
+    return this.http.delete<IColumn>(`boards/${boardId}/columns/${columnId}`);
+  }
+
+  updateColumn(boardId: string, columnId: string, title: string, order: number) {
+    return this.http.put<IColumn>(`boards/${boardId}/columns/${columnId}`, { title, order });
   }
 }
