0c71f962b0972df8b2a5cc78d6817b4a1ae40b92
VladimirParmon <parmon_vladimir@bk.ru>
2022-05-12 23:08:44 +0300

    add horizontal scrolling directive

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -6,9 +6,10 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { BoardPageComponent } from './pages/board-page/board-page.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { HorizontalScrollDirective } from './directives/horizontal-scroll.directive';
 
 @NgModule({
-  declarations: [MainPageComponent, BoardPageComponent],
+  declarations: [MainPageComponent, BoardPageComponent, HorizontalScrollDirective],
   imports: [CommonModule, BoardRoutingModule, SharedModule, TranslateModule.forChild()],
 })
 export class BoardModule {}
