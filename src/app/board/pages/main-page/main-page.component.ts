c56d8e0f167c1d4fd47affdaa88a478339715493
VladimirParmon <parmon_vladimir@bk.ru>
2022-05-13 19:17:52 +0300

    change main to board routing

--- src/app/board/pages/main-page/main-page.component.ts
+++ src/app/board/pages/main-page/main-page.component.ts
@@ -4,7 +4,7 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { select, Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { ConfirmationModalComponent } from 'src/app/shared/components/confirmation-modal/confirmation-modal.component';
-import { fetchBoards } from 'src/app/redux/actions/board.actions';
+import { fetchBoards, handleSaveOpenedBoardName } from 'src/app/redux/actions/board.actions';
 import { selectBoards } from 'src/app/redux/selectors/boards.selectors';
 import { MAStore, TBoards } from 'src/app/redux/models/store.model';
 import { BoardService } from '../../services/board.service';
@@ -14,6 +14,8 @@ import {
   DialogDataTitles,
 } from 'src/app/shared/constants/dialog.constants';
 import { CreateBoardDialogComponent } from 'src/app/shared/components/create-board-dialog/create-board-dialog.component';
+import { Router } from '@angular/router';
+import { UrlPaths } from 'src/app/shared/constants/url-paths';
 
 @Component({
   selector: 'ma-main-page',
@@ -25,7 +27,12 @@ export class MainPageComponent implements OnInit, OnDestroy {
 
   boards!: TBoards;
 
-  constructor(public dialog: MatDialog, private store: Store, public boardService: BoardService) {
+  constructor(
+    public dialog: MatDialog,
+    private store: Store,
+    public boardService: BoardService,
+    private router: Router
+  ) {
     this.observer$ = (this.store as Store<MAStore>)
       .pipe(select(selectBoards))
       .subscribe((state) => {
@@ -64,5 +71,10 @@ export class MainPageComponent implements OnInit, OnDestroy {
     };
 
     this.dialog.open(CreateBoardDialogComponent, dialogConfig);
+  }
+
+  handleRouting(boardName: string, boardId: string): void {
+    this.store.dispatch(handleSaveOpenedBoardName({ title: boardName }));
+    this.router.navigate([UrlPaths.board, boardId]);
   }
 }
